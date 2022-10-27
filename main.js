47b69c07485121c4a4a6465b0f53c3d5ddfbe68b
lisa-go <lis-zaa@live.com>
2022-10-27 04:56:07 -0500

    pointer locks and unlocks on win and restart

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-let pTurn = 0;
+const aContainer = document.querySelector('#content');
 
+let pTurn = 0;
 
 GameBoard = {
     board : ['','','',
@@ -13,8 +14,8 @@ GameBoard = {
 /* function to place alternate markers on each turn */
 function placeMarker(num) {
     return {
-        P1: 'X',
-        P2: 'O',
+        P1: 'X', /* star */
+        P2: 'O', /* heart */
 
         gridSpace: document.querySelector('#grid' + num),
         
@@ -22,7 +23,7 @@ function placeMarker(num) {
             if (GameBoard.board[num] == ''){
                 if (pTurn % 2 == 0){
                     GameBoard.board[num] = this.P1,
-                    this.gridSpace.textContent = this.P1,
+                    this.gridSpace.innerHTML = '<img src="./images/star.svg" class="filter-yellow">',
                     pTurn++
 
                     console.log(pTurn);
@@ -32,7 +33,7 @@ function placeMarker(num) {
 
                 else if (pTurn % 2 !== 0){
                     GameBoard.board[num] = this.P2,
-                    this.gridSpace.textContent = this.P2,
+                    this.gridSpace.innerHTML = '<img src="./images/heart.svg" class="filter-pink">',
                     pTurn++
 
                     console.log(pTurn);
@@ -50,116 +51,120 @@ function placeMarker(num) {
 
 /* function to determine if someone has won */
 function gameResult() {
+    const resultDisplay = document.createElement("resultDisplay");
+    resultDisplay.classList.add("resultDisplay");
+
     if (GameBoard.board[0] == 'X' && 
     GameBoard.board[1] == 'X' &&
     GameBoard.board[2] == 'X') {
-        console.log('P1: X wins');
-        clearBoard.forEach(endGame);
+        resultDisplay.textContent = 'Star wins!';
+        pointerLock();
+        
     }
 
     else if (GameBoard.board[3] == 'X' && 
     GameBoard.board[4] == 'X' &&
     GameBoard.board[5] == 'X') {
-        console.log('P1: X wins');
-        clearBoard.forEach(endGame);
+        resultDisplay.textContent = 'Star wins!';
+        pointerLock();
     }
 
     else if (GameBoard.board[6] == 'X' && 
     GameBoard.board[7] == 'X' &&
     GameBoard.board[8] == 'X') {
-        console.log('P1: X wins');
-        clearBoard.forEach(endGame);
+        resultDisplay.textContent = 'Star wins!';
+        pointerLock();
     }
 
     else if (GameBoard.board[0] == 'X' && 
     GameBoard.board[3] == 'X' &&
     GameBoard.board[6] == 'X') {
-        console.log('P1: X wins');
-        clearBoard.forEach(endGame);
+        resultDisplay.textContent = 'Star wins!';
+        pointerLock();
     }
 
     else if (GameBoard.board[1] == 'X' && 
     GameBoard.board[4] == 'X' &&
     GameBoard.board[7] == 'X') {
-        console.log('P1: X wins');
-        clearBoard.forEach(endGame);
+        resultDisplay.textContent = 'Star wins!';
+        pointerLock();
     }
 
     else if (GameBoard.board[2] == 'X' && 
     GameBoard.board[5] == 'X' &&
     GameBoard.board[8] == 'X') {
-        console.log('P1: X wins');
-        clearBoard.forEach(endGame);
+        resultDisplay.textContent = 'Star wins!';
+        pointerLock();
     }
 
     else if (GameBoard.board[2] == 'X' && 
     GameBoard.board[4] == 'X' &&
     GameBoard.board[6] == 'X') {
-        console.log('P1: X wins');
-        clearBoard.forEach(endGame);
+        resultDisplay.textContent = 'Star wins!';
+        pointerLock();
     }
 
     else if (GameBoard.board[0] == 'X' && 
     GameBoard.board[4] == 'X' &&
     GameBoard.board[8] == 'X') {
-        console.log('P1: X wins');
-        clearBoard.forEach(endGame);
+        resultDisplay.textContent = 'Star wins!';
+        pointerLock();
     }
 
     else if (GameBoard.board[0] == 'O' && 
     GameBoard.board[1] == 'O' &&
     GameBoard.board[2] == 'O') {
-        console.log('P2: O wins');
-        clearBoard.forEach(endGame);
+        resultDisplay.textContent = 'Heart wins!';
+        pointerLock();
     }
 
     else if (GameBoard.board[3] == 'O' && 
     GameBoard.board[4] == 'O' &&
     GameBoard.board[5] == 'O') {
-        console.log('P2: O wins');
-        clearBoard.forEach(endGame);
+        resultDisplay.textContent = 'Heart wins!';
+        pointerLock();
     }
 
     else if (GameBoard.board[6] == 'O' && 
     GameBoard.board[7] == 'O' &&
     GameBoard.board[8] == 'O') {
-        console.log('P2: O wins');
-        clearBoard.forEach(endGame);
+        resultDisplay.textContent = 'Heart wins!';
+        pointerLock();
     }
 
     else if (GameBoard.board[0] == 'O' && 
     GameBoard.board[3] == 'O' &&
     GameBoard.board[6] == 'O') {
-        console.log('P2: O wins');
-        clearBoard.forEach(endGame);
+        resultDisplay.textContent = 'Heart wins!';
+        pointerLock();
     }
 
     else if (GameBoard.board[1] == 'O' && 
     GameBoard.board[4] == 'O' &&
     GameBoard.board[7] == 'O') {
-        console.log('P2: O wins');
-        clearBoard.forEach(endGame);
+        resultDisplay.textContent = 'Heart wins!';
+        pointerLock();
     }
 
     else if (GameBoard.board[2] == 'O' && 
     GameBoard.board[5] == 'O' &&
     GameBoard.board[8] == 'O') {
-        console.log('P2: O wins');
-        clearBoard.forEach(endGame);
+        resultDisplay.textContent = 'Heart wins!';
+        pointerLock();
     }
 
     else if (GameBoard.board[2] == 'O' && 
     GameBoard.board[4] == 'O' &&
     GameBoard.board[6] == 'O') {
-        console.log('P2: O wins');
-        clearBoard.forEach(endGame);
+        resultDisplay.textContent = 'Heart wins!';
+        pointerLock();
     }
 
     else if (GameBoard.board[0] == 'O' && 
     GameBoard.board[4] == 'O' &&
     GameBoard.board[8] == 'O') {
-        console.log('P2: O wins');
-        clearBoard.forEach(endGame);
+        resultDisplay.textContent = 'Heart wins!';
+        pointerLock();
     }
 
     else if (GameBoard.board[0] !== '' &&
@@ -171,9 +176,11 @@ function gameResult() {
     GameBoard.board[6] !== '' &&
     GameBoard.board[7] !== '' &&
     GameBoard.board[8] !== '') {
-        console.log ('draw');
-        clearBoard.forEach(endGame);
+        resultDisplay.textContent = 'Draw!';
+        pointerLock();
     }
+
+    aContainer.appendChild(resultDisplay);
 }
 
 let clearBoard = document.querySelectorAll('grid');
@@ -187,3 +194,15 @@ function endGame(element) {
     element.textContent = '';
     pTurn = 0;
 }
+
+function restartGame() {
+    clearBoard.forEach(endGame);
+    document.body.style.setProperty("--pointer", "all");
+}
+
+/* toggle pointer lock on game end and game restart */
+function pointerLock() {
+    document.body.style.setProperty("--pointer", "none");
+  }
+
+  //remove resultdisplay text//
